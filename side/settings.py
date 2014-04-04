358c5b69d37fbbb0205878ec2f97c790a0ad8ea3
zhijiasun <zhijiasun84@gmail.com>
2014-04-05 07:24:28 +0800

    add the rest framework example

--- side/settings.py
+++ side/settings.py
@@ -85,3 +85,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
--- side/urls.py
+++ side/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from epm import views
 
 """
 from django.contrib import admin
@@ -9,11 +11,17 @@ xadmin.autodiscover()
 
 from xadmin.plugins import xversion
 xversion.register_models()
+
+router = routers.DefaultRouter()
+router.register(r'users',views.UserViewSet)
+router.register(r'groups',views.GroupViewSet)
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'side.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^xadmin/', include(xadmin.site.urls)),
+    url(r'^',include(router.urls)),
     url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework')),
 )
